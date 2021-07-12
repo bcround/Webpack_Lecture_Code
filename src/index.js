beee8f37dfad2b66b92e4feae9dfac6931e9f167
bcround <sbc1142@gmail.com>
2021-07-12 12:23:13 +0900

    Feat: SASS loader 설정 및 적용 modified:   package-lock.json modified:   package.json deleted:    src/index.css modified:   src/index.js new file:   src/index.module.scss modified:   webpack.common.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import 'normalize.css'; // import키만 사용하고 그뒤에 path의 값을 적어주면 모듈을 포함한 상태로 만들어 줄 수 있다.
-import styles from './index.css'; // css는 선택자의 이름이 전역 환경으로 적용되기 때문에 어플리케이션 규모가 커졌을때도 선택자 이름의 충돌이 없도록 신경써줘야 하는데,
+import styles from './index.module.scss'; // css는 선택자의 이름이 전역 환경으로 적용되기 때문에 어플리케이션 규모가 커졌을때도 선택자 이름의 충돌이 없도록 신경써줘야 하는데,
 //CSS Modules라는 설정을 해주면, css파일을 모듈로 불러오고 class이름들을 자바스크립트 파일에 직접 불러와서 사용할 수 있다.
 // 그래서 CSS Modules 설정을 하면 css파일별로 클래스 이름이 같아도 겹치지 않는다는 장점이 있다.
 import $ from 'jquery';
 import slackImg from './assets/slack_logo.jpeg';
+import slackImgSvg from './assets/slack-new-logo.svg';
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = 'Hello Webpack';
 
   const imgElement = document.createElement('img');
-  imgElement.src = slackImg;
+  imgElement.src = slackImgSvg;
 
   console.log(styles); //{helloWebpack: "Xsm8tCif7FknljylODIGx"}, css파일에 있는 클래스 이름이 객체 키로 전달이 되고 키의 값은 변형된 해시값이 할당된다.
 
-  console.log(slackImg);
+  console.log(slackImgSvg);
   element.appendChild(imgElement);
 
   element.classList = styles.helloWebpack;
--- webpack.common.js
+++ webpack.common.js
@@ -14,25 +14,39 @@ module.exports = {
   },
   module: {
     rules: [
+      // filename.module.scss => css modules, 이렇게 css파일 이름을 정의를 했을때 중간에 module이라는 글자가 들어가면, css modules가 적용이 되게 할 것이다. 그러면 module이 중간에 들어간 파일들의 class들은 지역화된 이름으로 변경이 되서 바뀌기 때문에 충돌하는 일이 발생하지 않는다.
       {
-        test: /\.css$/i,
-        use: [
-          // {
-          //   loader: 'style-loader',
-          // style-loader는 처리하는 css 파일별로 style tag를 만든다.
-          // style tag 하나에서 한 번에 스타일 정보들을 읽어올 수 있도록 하려면 style-loader options에서 injectType을 singletonStyleTag로 정해줘야 한다.
-          //   options: {
-          //     injectType: 'singletonStyleTag'
-          //   }
-          // },
+        // filename.scss => global, css module이 적용이 안된 global 형태가 되게끔 한다.
+        test: /\.s?css$/,
+        oneOf: [
+          // oneOf키는 여러 rule중의 하나의 rule이 작용이 되게끔 조건을 걸어주는 역할을 한다.
           {
-            loader: MiniCssExtractPlugin.loader
+            test: /\.module\.s?css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true
+                }
+              },
+              'sass-loader'
+            ]
           },
           {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
+            // 첫번째 test (module.scss) 룰을 따르지 않는 경우는 모두 이 두번째 rule을 따른다 (module을 적용하지 않은 상태, global 범위).
+            use: [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true
+                }
+              },
+              'sass-loader'
+            ]
           }
         ]
       },
@@ -56,6 +70,18 @@ module.exports = {
               }, // ext는 확장자(extension)의 약자이다.
               publicPath: 'assets/', // assets폴더 안의 파일들을 보겠다.
               outputPath: 'assets/' // 실제로 빌드가되고나서 file loader를 통해 file이 생성되는 경로를 개입할 수 있게 해주는 키, dist폴더내에 assets폴더를 만들어 asset들을 거기에 담겠다.
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // limit키 값은 byte크기 단위의 숫자가 들어간다. 파일크기의 제한을 걸 수 있다. 8KB까지만 url loader를 적용하겠다. 파일크기가 너무 컸을때는 base64로 인코딩된 문자열 형태가 엄청 커질 수도 있다.
+              limit: 8192 // 만약 파일크기가 limit을 걸어둔 값보다 넘게 되면, default로 file-loader를 사용한다. 혹은 options에 fallback이란 키로 무슨 loader를 사용할지 정해줄 수 있다.(alternative loader)
             }
           }
         ]
